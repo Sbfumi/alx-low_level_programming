2c1ed0160fdcfcb2c48aa39afb9a14c8bc40f275
minnie <missfuzelihle@gmail.com>
2023-03-28 17:50:02 -0700

    strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - Reverse strings
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int rev = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		rev++;
+		s++;
+	}
+	s--;
+	for (o = rev; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
